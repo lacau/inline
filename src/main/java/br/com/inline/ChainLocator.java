1e9c70786af115e8120433f35fe73f6a5da677e7
Lacau <maz_llacau@uolinc.com>
2016-06-08 21:23:36 -0300

    Changed to use already injected commands.

--- src/main/java/br/com/inline/ChainLocator.java
+++ src/main/java/br/com/inline/ChainLocator.java
@@ -1,20 +1,18 @@
 package br.com.inline;
 
 import java.util.Map;
+import javax.annotation.Resource;
 
 import org.apache.commons.chain.impl.ChainBase;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by lacau on 09/05/16.
  */
 @Component
-@DependsOn("registerChains")
 public final class ChainLocator {
 
-    @Autowired
+    @Resource(name = "chains")
     private Map<String, ChainBase> chains;
 
     public final ChainBase getChain(String chainName) {
--- src/main/java/br/com/inline/ChainSpringRegister.java
+++ src/main/java/br/com/inline/ChainSpringRegister.java
@@ -1,10 +1,17 @@
 package br.com.inline;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import br.com.inline.annotation.Chain;
+import org.apache.commons.chain.Command;
 import org.apache.commons.chain.impl.ChainBase;
 import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,16 +21,51 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChainSpringRegister {
 
-    @Bean
-    public String registerChains(ChainSpringUtils chainSpringUtils) {
-        Set<Class<?>> annotatedWith = new Reflections().getTypesAnnotatedWith(Chain.class);
+    @Autowired
+    private ApplicationContext context;
 
-        for(Class<?> clazz : annotatedWith) {
-            Chain chain = clazz.getAnnotation(Chain.class);
-            ChainBase chainBase = chainSpringUtils.getChain(chain);
-            chainSpringUtils.registerBean(chain.qualifier(), chainBase);
+    @Bean
+    public Map<String, Command> registerCommands() throws IllegalAccessException, InstantiationException {
+        final Map<String, Command> commands = new HashMap<>();
+        final Map<String, Object> beansWithAnnotation = context.getBeansWithAnnotation(br.com.inline.annotation.Command.class);
+        for(String beanName : beansWithAnnotation.keySet()) {
+            final Object command = beansWithAnnotation.get(beanName);
+            commands.put(beanName, (Command) command);
         }
 
-        return "";
+        return commands;
+    }
+
+    @Bean(name = "chains")
+    public Map<String, ChainBase> chains(Map<String, Command> commands) {
+        final Map<String, ChainBase> chains = new HashMap<>();
+
+        final Set<Class<?>> annotatedWith = new Reflections().getTypesAnnotatedWith(Chain.class);
+        for(Class<?> clazz : annotatedWith) {
+            Chain chain = clazz.getAnnotation(Chain.class);
+            chains.put(chain.qualifier(), getChain(chain, commands));
+        }
+
+        return chains;
+    }
+
+    private ChainBase getChain(Chain chain, Map<String, Command> commands) {
+        final List list = new LinkedList();
+
+        for(Class clazz : chain.commands()) {
+            list.add(commands.get(getClassName(clazz)));
+        }
+
+        return new ChainBase(list);
+    }
+
+    private String getClassName(Class clazz) {
+        final String className = clazz.getSimpleName();
+
+        if(className.length() > 1 && Character.isUpperCase(className.charAt(1))) {
+            return className;
+        }
+
+        return className.substring(0, 1).toLowerCase() + className.substring(1);
     }
 }
